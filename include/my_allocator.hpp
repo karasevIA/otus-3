40bc9f53f007bc65faf7e8c505a9159de4f1081d
karasevIA <karasev.ia@phystech.edu>
2022-09-18 22:52:27 +0300

    first normal work allocator

--- include/my_allocator.hpp
+++ include/my_allocator.hpp
@@ -1,87 +1,145 @@
 #pragma once
 
-#include <cstddef>
-#include <memory>
+#include <iostream>
+
+namespace {
+    struct MemoryNode {
+        MemoryNode* next;
+        std::size_t size;
+        std::size_t capacity;
+        void* data;
+        MemoryNode(std::size_t cap, void* dat) : capacity(cap), data(dat), next(nullptr) {}
+        MemoryNode(MemoryNode* n) : next(n) {}
+    };
+
+    struct MemoryBank {
+        MemoryNode* list = nullptr;
+        std::size_t used = 0;
+        std::size_t allocated = 0;
+        void* alloc(std::size_t bytes) {
+            if (list == nullptr) {
+                void* p = std::malloc(sizeof(MemoryNode) + bytes);
+                if (!p)
+                    throw std::bad_alloc();
+                list = new (p) MemoryNode(bytes, (char*)p + sizeof(MemoryNode));
+                list->size = bytes;
+                allocated += bytes;
+                used += bytes;
+                return list->data;
+            }
+            MemoryNode tmp(list);
+            MemoryNode* current = &tmp;
+            do {
+                current = current->next;
+                if (current->size == 0 && current->capacity >= bytes) {
+                    current->size = bytes;
+                    used += bytes;
+                    return current->data;
+                }
+            } while (current->next != nullptr);
+            
+            void* p = std::malloc(sizeof(MemoryNode) + bytes);
+            if (!p)
+                throw std::bad_alloc();
+            current->next = new (p) MemoryNode(bytes, (char*)p + sizeof(MemoryNode));
+            current->next->size = bytes;
+            allocated += bytes;
+            used += bytes;
+            return current->next->data;
+        }
+        
+        void free(void* p) {
+            if (p == nullptr)
+                throw std::bad_alloc();
+
+            MemoryNode tmp(list);
+            MemoryNode* current = &tmp;
+            do {
+                current = current->next;
+                if (current->data == p) {
+                    used -= current->size;
+                    current->size = 0;
+                    return; // success free
+                }
+            } while (current->next != nullptr);
+
+            throw std::bad_alloc();
+        }
+
+        void destruct() {
+            MemoryNode* current = list;
+            do {
+                MemoryNode* next = current->next;
+                std::free(current);
+                current = next;
+            } while (current->next != nullptr);
+            std::free(current);
+        }
+
+        void print() {
+            MemoryNode tmp(list);
+            MemoryNode* current = &tmp;
+            do {
+                current = current->next;
+                std::cout << "[" << current->size << "/" << current->capacity << "]->";
+            } while (current->next != nullptr);
+            std::cout << "|  " << used << "/" << allocated << std::endl;
+        }
+    };
+}
 
 namespace My {
-	template <typename T>
-	class Allocator {
-	public:
-		using value_type = T;
-
-		Allocator();
-		~Allocator();
-
-		template <typename U>
-		Allocator(const Allocator<U>&);
-
-		T* allocate (std::size_t n);
-		void deallocate (T* p, std::size_t n);
-		
-		static void set_block_size(std::size_t s) { if (s > 0) block_size = s; }
-	private:
-		void _deallocate();
-		void _allocate(std::size_t blocks);
-
-		inline static std::size_t block_size = 10;
-		void* pointer;
-		std::size_t allocated = 0;
-	};
-}
-
-
-#include <iostream>
-#include <memory>
-#include <vector>
-
-template <typename T>
-My::Allocator<T>::Allocator() {
-    _allocate(1);
-}
-
-template <typename T>
-My::Allocator<T>::~Allocator() {
-    std::free(pointer);
-}
-
-template <typename T>
-template <typename U>
-My::Allocator<T>::Allocator(const Allocator<U>& ) {
-    // should we make a copy of the rhs.m_buffer ?
-    // No, we should not!
-}
-
-template <typename T>
-T* My::Allocator<T>::allocate (std::size_t n) {
-    if (n > allocated) {
-        _deallocate();
-        std::size_t blocks = n / block_size;
-        if (n > blocks * block_size) {
-            blocks += 1;
+    template <typename T>
+    struct Allocator {
+        using value_type = T;
+        Allocator() {
+            //std::cout << "constructor" << std::endl;
+            bank = reinterpret_cast<MemoryBank*>(std::calloc(1, sizeof(MemoryBank)));
+            std::size_t bs = block_size ? block_size : default_block_size;
+            T* p1 = allocate(bs);
+            T* p2 = allocate(bs);
+            deallocate(p1, bs);
+            deallocate(p2, bs);
+            //bank->print();
         }
-        _allocate(blocks);
-    }
-    return reinterpret_cast<T*>(pointer);
-}
 
-template <typename T>
-void My::Allocator<T>::_deallocate() {
-    auto tmp = pointer;
-    pointer = nullptr;
-    allocated = 0;
-}
-	
-template <typename T>
-void My::Allocator<T>::_allocate(std::size_t blocks) {
-    pointer = std::malloc(block_size * blocks * sizeof(T));
-    if (!pointer)
-        throw std::bad_alloc();
-    allocated = block_size * blocks;
-}
+        ~Allocator() {
+            //std::cout << "destructor " << bank->used << "/" << bank->allocated << std::endl;
+            if (bank->allocated == 0) {
+                bank->destruct();
+            }
+        }
 
-template <typename T>
-void My::Allocator<T>::deallocate (T*, std::size_t) {
-    _deallocate();
+        Allocator(const Allocator& A) {
+            //std::cout << "copy constructor" << std::endl;
+            bank = A.bank;
+        }
+
+        template <typename U>
+        Allocator(const Allocator<U>&) {}
+
+        T* allocate (std::size_t n) {
+            //std::cout << "allocate " << n << std::endl;
+            return reinterpret_cast<T*>(bank->alloc(sizeof(T) * n));
+        }
+
+        void deallocate (T* p, std::size_t n) {
+            //std::cout << "dealocate " << n << std::endl;
+            bank->free(p);
+            //bank->print();
+        }
+
+        void print() {
+            bank->print();
+        }
+
+        static void set_block_size(std::size_t s) { if (s > 0) block_size = s; }
+        
+    private:
+        static constexpr std::size_t default_block_size = 10;
+        inline static std::size_t block_size = 10;
+        MemoryBank* bank;
+    };
 }
 
 template <class T, class U>
--- source/main.cpp
+++ source/main.cpp
@@ -7,16 +7,13 @@ int main()
 {
     std::cout << "Hello" << std::endl;
     
-	My::Allocator<int>::set_block_size(10);
+	My::Allocator<int>::set_block_size(16);
 	std::cout << "Init vector" << std::endl;
 	std::vector<int, My::Allocator<int>> vector;
 
 	for (int i = 0; i < 10; ++i) {
 		vector.push_back(i);
-		for (const auto& v : vector) {
-			std::cout << v << ' ';
-		}
-		std::cout << std::endl;
+		//vector.get_allocator().print();
 	}
 
 	for (const auto& v : vector) {
@@ -25,11 +22,11 @@ int main()
 	std::cout << std::endl;
 
 	auto other = vector;
-
-	for (const auto& v : other) {
-		std::cout << v << ' ';
-	}
+	///std::cout << "aaaa" << std::endl;
+	//for (const auto& v : other) {
+	//	std::cout << v << ' ';
+	//}
 	std::cout << std::endl;
-    
+
     return 0;
 }
